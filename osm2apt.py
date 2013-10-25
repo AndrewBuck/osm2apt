bff5e53817144c919ccbdf89cbc4266043090cf4
andrewbuck40 <andrewbuck40@users.sourceforge.net>
2013-10-24 20:38:30 -0500

    Adding function to cut line at specified length.

--- osm2apt.py
+++ osm2apt.py
@@ -208,6 +208,23 @@ def printPolygon(area):
 
     return ret
 
+def cut(line, distance):
+    # Cuts a line in two at a distance from its starting point
+    if distance <= 0.0 or distance >= line.length:
+        return [LineString(line)]
+    coords = list(line.coords)
+    for i, p in enumerate(coords):
+        pd = line.project(Point(p))
+        if pd == distance:
+            return [
+                LineString(coords[:i+1]),
+                LineString(coords[i:])]
+        if pd > distance:
+            cp = line.interpolate(distance)
+            return [
+                LineString(coords[:i] + [(cp.x, cp.y)]),
+                LineString([(cp.x, cp.y)] + coords[i:])]
+
 def isAbandoned(tags):
     if 'abandoned' in tags:
         if tags['abandoned'] == 'yes':
