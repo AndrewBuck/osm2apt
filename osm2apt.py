c6ee213bd0b56f641621799172aa8ab59bd8d6ef
andrewbuck40 <andrewbuck40@users.sourceforge.net>
2013-10-26 14:43:58 -0500

    Adding code to fill in small islands of grass in curved taxiway intersections with pavement.

--- osm2apt.py
+++ osm2apt.py
@@ -521,6 +521,29 @@ class Aerodrome(SpatialObject):
             else:
                 self.taxiwaySurfaces[taxiway.surfaceInteger] = taxiway.concreteGeometry
 
+            # Now that the taxiways have been combined as such, loop over and
+            # remove small islands in the concrete surfaces (generally these
+            # are small triangular regions formed between a 'Y' connection
+            # which connects to another taxiway across the top of the 'Y').
+            newSurfaces = {}
+            minArea = math.pow(metersToDeg(30.0), 2)
+            for surfaceType, surface in self.taxiwaySurfaces.items():
+                newPolygons = []
+                if isinstance(surface, MultiPolygon):
+                    for geom in surface.geoms:
+                        newRingList = []
+                        for ring in geom.interiors:
+                            if Polygon(ring).area >= minArea:
+                                newRingList.append(ring)
+
+                        newPolygons.append(Polygon(geom.exterior, newRingList))
+
+                    newSurfaces[surfaceType] = MultiPolygon(newPolygons)
+                else:
+                    newSurfaces[surfaceType] = surface
+
+            self.taxiwaySurfaces = newSurfaces
+
             # Loop over each pair of taxiways and use a buffer along the center
             # of each taxiway to trim out the shoulder markings on the other
             # one; this cleans up the intersections.
@@ -1107,8 +1130,11 @@ class Osm2apt_class(object):
         for ls, obj in objectsToRemove:
             ls.remove(obj)
 
+        print '\tClean geometries...'
         for a in self.aerodromes:
             a.cleanGeometries()
+
+        print '\tDone.'
 
 # Main function
 print 'osm2apt version 0.4.1'
